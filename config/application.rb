cd2fca26ccd84006d4d2a6f8811e947d57cbc723
Neves <marcos.neves@gmail.com>
2019-04-07 09:40:37 -0300

    choose rails frameworks

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,19 @@
 
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+# require "active_job/railtie"
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+# require "action_cable/engine"
+# require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,7 +25,6 @@ module RailsEstudos
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Brasilia'
-    config.assets.enabled = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
