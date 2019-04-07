080eba448412d95b2f1415239daf327036a994b6
Neves <marcos.neves@gmail.com>
2019-04-07 08:33:21 -0300

    M app/controllers/lab_controller.rb A docs/second-heroku-deploy.log

--- app/controllers/lab_controller.rb
+++ app/controllers/lab_controller.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
 class LabController < ApplicationController
   def delay
     time = Time.zone.now
-    time = [time, time.to_datetime.strftime("%Q").last(3)].join "."
+    time = [time, time.to_datetime.strftime('%Q').last(3)].join '.'
     sleep params[:seconds]&.to_i || 1
     render plain: time
   end
 
   def env
     data = {
+      request_id: request.request_id,
       ENV: ENV.sort.to_h,
-      headers: headers,
+      env: request.env.keys,
+      headers: headers
     }
     render json: data
   end
